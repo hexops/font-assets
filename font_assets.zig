ab288c8ceee528bd149f3946061e50ad778dee88
LordMZTE <lord@mzte.de>
2023-04-03 00:13:34 +0200

    add content

---
+++ font_assets.zig
@@ -0,0 +1,3 @@
+pub const deja_vu_sans_pfb = @embedFile("fonts/DejaVuSans.pfb");
+pub const deja_vu_sans_pfm = @embedFile("fonts/DejaVuSans.pfm");
+pub const fira_sans_regular_ttf = @embedFile("fonts/FiraSans-Regular.ttf");
