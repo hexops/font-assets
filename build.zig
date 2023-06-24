617ab30ce559841199b6c5af74a7693137c0535b
Stephen Gutekanst <stephen@hexops.com>
2023-06-24 14:20:18 -0700

    cleanup project structure; add license info
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("font_assets", .{
-        .source_file = .{ .path = "font_assets.zig" },
+    _ = b.addModule("font-assets", .{
+        .source_file = .{ .path = "main.zig" },
     });
 }
---
+++ main.zig
@@ -0,0 +1,3 @@
+pub const deja_vu_sans_pfb = @embedFile("fonts/dejavu/DejaVuSans.pfb");
+pub const deja_vu_sans_pfm = @embedFile("fonts/dejavu/DejaVuSans.pfm");
+pub const fira_sans_regular_ttf = @embedFile("fonts/firasans/FiraSans-Regular.ttf");
