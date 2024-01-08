626877b1350628ac7411344ef02ae4b58affa656
Palsmo <johannesedvingreen@gmail.com>
2024-01-08 20:14:02 +0100

    update to Zig 2024.1.0-mach

--- build.zig
+++ build.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("font-assets", .{
-        .source_file = .{ .path = "main.zig" },
+        .root_source_file = .{ .path = "main.zig" },
     });
 }
