d7708a0a1b0788b26ec68ab087084721b2908bf2
Ali Chraghi <alichraghi@proton.me>
2024-01-08 19:44:38 +0330

    update to latest build API

--- build.zig
+++ build.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("font-assets", .{
-        .source_file = .{ .path = "main.zig" },
-    });
+    _ = b.addModule("font-assets", .{ .root_source_file = .{ .path = "main.zig" } });
 }
